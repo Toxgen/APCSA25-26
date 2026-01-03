8d66722043d1612727da575bcf1de2df20cbccc1
= <=>
2026-01-02 23:00:58 -0500

    first commit

---
+++ unit_1/hello_world.java
@@ -0,0 +1,12 @@
+public class hello_world
+{
+    public static void main(String args[])
+    {
+        // standard way of printing hello world
+        System.out.println("hello world");
+
+        // another way to print hello world
+        System.out.print("hello");
+        System.out.println(" world");
+    }
+}
